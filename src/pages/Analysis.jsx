f2864f330954c108d84ccddeca4ab9c5b038312e
iamaftab12 <marofficial1205@gmail.com>
2026-07-28 11:24:40 +0530

    source structure added

---
+++ src/pages/Analysis.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react';
+
+function Analyis(){
+    return(
+        <div>
+            <h1>Welcome to the Analysis</h1>
+            <p>This is the analysis page of our application.</p>
+        </div>
+    );
+}
+
+export default Analysis;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react';
+
+function Projects(){
+    return(
+        <div>
+            <h1>Welcome to the Projects</h1>
+            <p>This is the projects page of our application.</p>
+        </div>
+    );
+}
+
+export default Projects;
